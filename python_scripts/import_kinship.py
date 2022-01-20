afa0676d8f7d15943ac0c50ed5e8f0282f0bc6be
filippob <filippo.biscarini@gmail.com>
2022-01-20 14:50:35 +0100

    creating separate scripts to load kinship and phenotypes

---
+++ python_scripts/import_kinship.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 17:00:47 2021
+
+@author: filippo
+"""
+
+"""
+Python script that uses import functions to download data files from a remote URL
+and then reads them into Python
+These files are amtrices (2D arrays) that are then stacked together
+into a 3D array
+
+By changing the variable part of the parameter 'base_dir'
+you can store kinship data files (with the same name structure)
+into different subfolders (e.g. /cattle/)
+"""
+
+import os
+import argparse
+from import_functions import make_filenames, download_files, stack_kinship
+
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Import data for deep learning')
+
+# Add arguments
+parser.add_argument('-r', '--remote_folder', type=str, required=True, 
+                    help='name of the remote folder from where sorted kinship matrices are downloaded')
+parser.add_argument('-s', '--target_dir', type=str, required=True, 
+                    help='directory where data are to be stored (created if needed)')
+parser.add_argument('-d', '--dataset', type=str, required=True, 
+                    help='dataset (e.g. cattle, maize, tropical_maize, etc.)')
+# Parse the argument
+args = parser.parse_args()
+
+# Print to check arguments values
+print('Remote folder is:', args.remote_folder)
+print('Target folder is:', args.target_dir)
+print('Dataset is:', args.target_dir)
+
+#### Set up of parameters and libraries
+## SETTINGS #######################
+
+# where the kinship data are stored
+#remote_data_folder = 'http://www.jackdellequerce.com/data/cattle/kinships_sorted/'
+remote_data_folder = os.path.join(args.remote_folder,'')
+
+# where to place the data
+#downloaded_data = '/content/data/'
+downloaded_data = args.target_dir
+#downloaded_data = '/home/filippo/Documents/deep_learning_for_breeding/'
+
+# specific dataset
+#dataset='cattle/'
+dataset = args.dataset
+
+#base_dir = downloaded_data + dataset
+base_dir = os.path.join(downloaded_data, dataset, '')
+####################################
+
+
+print('Data will be downloaded to:', base_dir)
+
+# ### Load the genotype data
+#
+# Kinship matrices are read from gzipped csv files, and then stacked together in a 3D array
+
+make_filenames()
+download_files(target_dir=base_dir, remote_data_folder=remote_data_folder)
+kinship = stack_kinship(base_dir=base_dir)
+
+print("the object 'kinship' has been created, with dimensions {}".format(kinship.shape))
+print("Kinship has been loaded!")
+
---
+++ python_scripts/import_phenotype.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 17:00:47 2021
+
+@author: filippo
+"""
+
+"""
+Python script that uses import functions to download data files from a remote URL
+and then reads them into Python
+The final phenotype data is a 1d array (sorted) or a 2d pandas dataframe 
+with ['sample_name', 'trait']
+
+If set is_sorted to True (default) the sorted phenotypes are loaded
+"""
+
+import os
+import argparse
+from import_functions import download_phenotype_files, load_phenotypes_and_select_trait
+
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Import data for deep learning')
+
+# Add arguments
+parser.add_argument('-r', '--remote_folder', type=str, required=True, 
+                    help='name of the remote folder from where sorted kinship matrices are downloaded')
+parser.add_argument('-s', '--target_dir', type=str, required=True, 
+                    help='directory where data are to be stored (created if needed)')
+parser.add_argument('-d', '--dataset', type=str, required=True, 
+                    help='dataset (e.g. cattle, maize, tropical_maize, etc.)')
+parser.add_argument('--sorted', type=bool, required=False, default=True,
+                    help='should the sorted (default) or unsorted phenotype data be downloaded?')
+parser.add_argument('-p', '--trait', type=str, required=True,
+                    help='Name of the trait to use for predictions (column of the phenotype file)')
+parser.add_argument('--outtype', type=str, required=False, default=False,
+                    help='Should the function return a 2d Pandas dataframe (True) or a 1d numpy array (False, default)')
+# Parse the argument
+args = parser.parse_args()
+
+# Print to check arguments values
+print('Remote folder is:', args.remote_folder)
+print('Target folder is:', args.target_dir)
+print('Dataset is:', args.target_dir)
+print('Sorted or not?', 'sorted' if args.sorted == True else 'non sorted')
+print('Selected trait:', args.trait)
+print('Output type:', '2d Pandas dataframe' if args.outtype == True else '1d numpy array')
+
+#### Set up of parameters and libraries
+## SETTINGS #######################
+
+# where the kinship data are stored
+#DATAFOLDER_URL = 'http://www.jackdellequerce.com/data/cattle/phenotypes/'
+remote_data_folder = os.path.join(args.remote_folder,'')
+
+# where to place the data
+#downloaded_data = '/content/data/'
+downloaded_data = args.target_dir
+#downloaded_data = '/home/filippo/Documents/deep_learning_for_breeding/'
+
+# specific dataset
+#dataset='cattle/'
+dataset = args.dataset
+
+#base_dir = downloaded_data + dataset
+base_dir = os.path.join(downloaded_data, dataset, '')
+####################################
+
+
+print('Data will be downloaded to:', base_dir)
+
+# ### Load the phenotype data
+#
+# The phenotype data are read from a csv file; the desired trait is then selected, 
+# and an ordered one-dimensional array (or a `Pandas` dataframe with two columns: `sample` and `trait`) 
+# is created where the phenotype data are stored
+
+download_phenotype_files(base_dir, remote_data_folder, is_sorted=args.sorted)
+
+#trait = 'simphe_mean0_hSquare0.7_cv0.1_QTN100_A10_D0_AA0_AD0_DA0_DD0_epoch1642079623'
+trait = args.trait
+phenotypes = load_phenotypes_and_select_trait(base_dir, trait, is_sorted=args.sorted, 
+                                              df_output=args.outtype)
+
+#print(phenotypes)
+print("Data have been loaded!")
