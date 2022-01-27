b899439b8e7f3c935303f8392ad67b941bff088e
ne1s0n <nelson.nazzicari@gmail.com>
2022-01-27 17:05:41 +0100

    implemented dictionary interface

--- python_scripts/keras_toolbox.py
+++ python_scripts/keras_toolbox.py
@@ -24,36 +24,64 @@ def plot_loss_history(h, metric = 'loss', outfile = None):
 	if outfile is not None:
 		pyplot.savefig(outfile)
 	pyplot.show()
-
+	
+	
+	
 #instantiate a network, which will then need to be compiled
-def instantiate_network(input_shape, conv_section = [32, 64], dense_section = [128], conv_kernel = (3, 3), conv_padding = 'same', maxpooling_kernel = (2,2), dropout_rate = 0.25):
+#default parameters:
+# - input_shape : this is required
+# - conv_layers = [32, 64]
+# - dense_layers = [128]
+# - conv_filter = (3, 3)
+# - conv_padding = 'same'
+# - pool_filter = (2,2)
+# - pool_step = 2
+# - drop_rate = 0.25
+def instantiate_network(config_dict):
+	
+	#cleanup of the config dictionary, so that we use local variables
+	input_shape  = config_dict['input_shape']
+	conv_layers  = config_dict.get('conv_layers', [32, 64]).copy() #copied so that we can pop
+	conv_filter  = config_dict.get('conv_filter', (3, 3))
+	conv_padding = config_dict.get('conv_padding', 'same')
+	dense_layers = config_dict.get('dense_layers', [128])
+	pool_filter  = config_dict.get('pool_filter', (2,2))
+	pool_step    = config_dict.get('pool_step', 2)
+	drop_rate    = config_dict.get('drop_rate', 0.25)
+	
+	#building the model
 	model = Sequential()
+	
+	#step counter for the maxpooling layers
+	mpstep = 0
 	
 	#first node is different, because it requires input shape
 	model.add(Conv2D(
-		conv_section.pop(0), 
-		kernel_size=conv_kernel,
+		conv_layers.pop(0), 
+		kernel_size=conv_filter,
 		activation='relu',
 		input_shape=input_shape, 
 		padding=conv_padding
 	))
-	model.add(MaxPooling2D(pool_size=maxpooling_kernel))
-	model.add(Dropout(dropout_rate))
+	mp_cnt += 1
+	model.add(Dropout(drop_rate))
 
-	
 	#convolutionary section
-	for nodes in conv_section:
-		model.add(Conv2D(nodes, conv_kernel, activation='relu', padding=conv_padding))
-		model.add(MaxPooling2D(pool_size=maxpooling_kernel))
-		model.add(Dropout(dropout_rate))
+	for nodes in conv_layers:
+		model.add(Conv2D(nodes, conv_filter, activation='relu', padding=conv_padding))
+		mp_cnt += 1
+		if mp_cnt >= pool_step:
+			model.add(MaxPooling2D(pool_size=pool_filter))
+			mp_cnt = 0
+		model.add(Dropout(drop_rate))
 		
 	#a flatten layer separates conv section from dense section
 	model.add(Flatten())
 	
 	#dense section
-	for nodes in dense_section:
+	for nodes in dense_layers:
 		model.add(Dense(nodes, activation='relu'))
-		model.add(Dropout(dropout_rate))
+		model.add(Dropout(drop_rate))
 	
 	#final output
 	model.add(Dense(1, activation='linear'))
