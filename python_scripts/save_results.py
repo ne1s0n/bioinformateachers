13515e7642491bd73927139305b9fae1a0859de4
filippob <filippo.biscarini@gmail.com>
2022-01-27 11:40:19 +0100

    changing the proportion of epochs used to average results

--- python_scripts/save_results.py
+++ python_scripts/save_results.py
@@ -17,7 +17,8 @@ def parse_history(h, phenotypes, val_split, trait):
     ## get metrics from history object
     df = pd.DataFrame(h.history)
     n_epochs = df.shape[0]
-    temp = df.iloc[int(n_epochs/2):(n_epochs-1),:].mean(axis=0)
+    threshold = int(n_epochs*0.9)
+    temp = df.iloc[threshold:(n_epochs-1),:].mean(axis=0)
     temp = temp.to_frame()
     temp.rename(columns={0: 'value'}, inplace=True)
     temp = temp.transpose()
