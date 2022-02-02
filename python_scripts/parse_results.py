b8a4def24b96e0871e9c09d9034015db67eee6c4
filippob <filippo.biscarini@gmail.com>
2022-02-02 16:20:13 +0100

    adding script with functions to parse results

---
+++ python_scripts/parse_results.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  2 14:54:22 2022
+
+@author: filippo
+"""
+
+""" Function(s) to parse results files from deep learning runs """
+
+#%% libraries
+import os
+import re
+import json
+import pandas as pd
+
+#%% function to read results and return a Pandas dataframe for further analysis
+def parse_results(filepath):
+    
+    basename = os.path.basename(filepath)
+    basefolder = os.path.dirname(filepath)
+    
+    print("Reading file '{}' from folder '{}'".format(basename, basefolder))
+    temp = pd.read_csv(filepath)
+    temp = temp.drop(temp.columns[[0]], axis=1)
+    curr_cols = [x for x in temp.columns]  
+    
+    print(" - making new columns")
+    new_cols = set()
+    for row in temp['config']:
+        rr = json.loads(row)
+        for naam in rr.keys():
+            if not naam in curr_cols: 
+                new_cols.add(naam)
+    
+    print(" - populating new columns")
+    config_res = dict() 
+    for row in temp['config']:
+        rr = json.loads(row)
+        for k,v in rr.items():
+            if isinstance(v, list):
+                val = '_'.join([repr(x) for x in v])
+            else:
+                val = repr(v)
+                
+            config_res.setdefault(k, []).append(val)
+
+    temp = temp.drop('config', axis=1)
+    res = pd.concat([temp.reset_index(drop=True), pd.DataFrame(config_res)], axis=1)
+    
+    print(" - returning dataframe of results")
+    
+    return res
+
+
