e5358593af13e98409c7456f2bb0fed344b5aba6
ne1s0n <nelson.nazzicari@gmail.com>
2022-02-02 17:33:17 +0100

    plot now supporting optional title

--- python_scripts/keras_toolbox.py
+++ python_scripts/keras_toolbox.py
@@ -15,12 +15,15 @@ from keras.regularizers import l1, l2, l1_l2
 
 #creates a plot with the required metric from the object returned 
 #by .fit() function. If an outfile if passed, the figure is saved
-#before invoking pyplot .show() 
-def plot_loss_history(h, metric = 'loss', outfile = None):
+#before invoking pyplot .show()
+#If a title is not present we report the metric 
+def plot_loss_history(h, metric = 'loss', outfile = None, title = None):
 	pyplot.plot(h.history[metric], label = 'Train ' + metric)
 	pyplot.plot(h.history['val_' + metric], label = 'Validation ' + metric)
 	pyplot.xlabel('Epochs')
-	pyplot.title(metric)
+	if title is None:
+		title = metric
+	pyplot.title(title)
 	pyplot.legend()
 
 	if outfile is not None:
