97268387d02d535ac67dfb5258d4102a46e85480
ne1s0n <nelson.nazzicari@gmail.com>
2022-02-16 12:53:52 +0100

    implemented merge_history

--- python_scripts/data_augmentation_toolbox.py
+++ python_scripts/data_augmentation_toolbox.py
@@ -44,5 +44,34 @@ def augment_add_normal_noise(x, y, reps=1, mu=0, sigma=0.1):
 		result_y = np.concatenate((result_y, y_now))
 	return(result_x, result_y)
 
-def merge_history(train_set_history, y, y_hat, past_merged_history = None):
-	return(None)
+#creates or updates a class object that mimicks what is returned by
+#keras model.fit() method, so that it's feedable to parse_history()
+#train_set_history   : returned by model.fit() on train data
+#val_set_evaluation  : returned by model.evaluate() on validation data
+#metrics             : list of names for the measured metrics, taken from model.metrics_names
+#past_merged_history : the object to be updated (from a previous call of this very function)
+def merge_history(train_set_history, val_set_evaluation, metrics, past_merged_history = None):
+	
+	#support class, in case we don't have a past history to enlarge
+	class MockTrainingHistory:
+		history = {}
+		
+	if past_merged_history is None:
+		past_merged_history = MockTrainingHistory()
+		
+	#at this point all new data should be added to past_merged_history
+	for i in range(len(metrics)):		
+		#retrieving the label
+		met = metrics[i]
+		
+		#adding the last value from training set
+		values = past_merged_history.history.get(met, [])
+		values.append(train_set_history.history[met][-1])
+		past_merged_history.history[met] = values
+		
+		#adding the proper value from validation set
+		values = past_merged_history.history.get('val_' + met, [])
+		values.append(val_set_evaluation[i])
+		past_merged_history.history['val_' + met] = values
+	
+	return(past_merged_history)
