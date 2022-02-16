851f1bb52cedf9af81b755fa3f9e9f1a0ea058f2
ne1s0n <nelson.nazzicari@gmail.com>
2022-02-16 16:12:47 +0100

    bugfix, now supporting variable number of columns

--- python_scripts/parse_results.py
+++ python_scripts/parse_results.py
@@ -34,20 +34,34 @@ def parse_results(filepath, outfilepath = None):
             if not naam in curr_cols: 
                 new_cols.add(naam)
     
+    print(new_cols)
     print(" - populating new columns")
     config_res = dict() 
     for row in temp['config']:
+		#local copy of the list of variable params
+        new_cols_cur = new_cols.copy()
+		
+		#parsing the values in the current row
         rr = json.loads(row)
         for k,v in rr.items():
+            new_cols_cur.remove(k)
             if isinstance(v, list):
                 val = '_'.join([repr(x) for x in v])
             else:
                 val = repr(v)
-                
             config_res.setdefault(k, []).append(val)
+        
+        #adding the columns that were not present in this row as blanks
+        for k in new_cols_cur:
+            config_res.setdefault(k, []).append(None)
+         
 
     temp = temp.drop('config', axis=1)
-    res = pd.concat([temp.reset_index(drop=True), pd.DataFrame(config_res)], axis=1)
+    
+    #putting together fixed and variable columns
+    a = temp.reset_index(drop=True)
+    b = pd.DataFrame(config_res)
+    res = pd.concat([a, b], axis=1)
     
     #should we save a csv?
     if outfilepath is not None:
