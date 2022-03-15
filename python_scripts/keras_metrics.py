fa8b0790596c309e25fd341ca557ad40443d1a22
filippob <filippo.biscarini@gmail.com>
2022-03-15 18:02:41 +0100

    first implementation of the function for the NDCG metric - using keras backend for sums, log with tensors - TODO: find a way to calculate log2 instead of log - TODO: find a way to sort tensors

--- python_scripts/keras_metrics.py
+++ python_scripts/keras_metrics.py
@@ -6,6 +6,8 @@ Created on Thu Jan 20 17:14:30 2022
 
 """ A collection of custom metrics for keras """
 
+import math
+import numpy as np
 import keras.backend as KB
 
 #pearson's correlation
@@ -26,3 +28,26 @@ def pearson(x, y):
 #Root Mean Square Error, to ease comparison with GROAN
 def rmse(x, y):
   return KB.mean(KB.sqrt((x - y) ** 2))
+
+## NDCG: normalised discounted cumulative gain
+def ndcg(y, y_hat, k=0.20):
+    
+    print('the selected value for k is {}'.format(k))
+    n = len(y) # number of predicted examples
+    nk = round(k*n) ## select the k top examples
+    print(nk)
+    y_inds = y.argsort()
+    y_sort_y = y[y_inds]
+    y_hat_inds = y_hat.argsort()
+    y_sort_y_hat = y[y_hat_inds]
+    
+    temp = 0
+    for i in range(nk):
+        ix = i+1
+        d = 1/(KB.log(ix)+1) ## should be log2 !! (TODO: find if with keras.backend this is possible)
+        num = KB.sum(y_sort_y_hat[0:ix])*d
+        den = KB.sum(y_sort_y[0:ix])*d
+
+        temp = (i*temp + num/den ) / (ix)
+    
+    return(temp)
