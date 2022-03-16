953fb76e94530913f4ca8a29a01deba9ef49f520
filippob <filippo.biscarini@gmail.com>
2022-03-16 17:05:41 +0100

    naive implementation with no k

--- python_scripts/keras_metrics.py
+++ python_scripts/keras_metrics.py
@@ -69,3 +69,28 @@ def ndcg_100(y, yhat):
     
     return(ndcg(y, yhat, 1.0))
     
+def ndcg_nok(y, y_hat):
+    
+    k = 1.0
+    kt = tf.convert_to_tensor(k, dtype=tf.float32)
+    nt = tf.convert_to_tensor(len(y), dtype=tf.int32) # number of predicted examples
+    nt = tf.cast(nt, dtype=tf.float32)
+    ## select the k top examples
+    nk = tf.math.round(kt*nt)
+    
+    y_inds = tf.argsort(y, direction='DESCENDING')
+    y_sort_y = tf.gather(y, y_inds)
+    y_hat_inds = tf.argsort(y_hat, direction='DESCENDING')
+    y_sort_y_hat = tf.gather(y, y_hat_inds)
+    
+    seq = tf.range(1.0, nk+1)
+    d = KB.log(seq+1)
+    k2 = KB.log(2.0)
+    d = k2/d ## 1/(d/k2) --> 1* k2/d
+    
+    num = KB.sum(y_sort_y_hat*d)
+    den = KB.sum(y_sort_y*d)
+
+    temp = num/den
+    
+    return(temp)
