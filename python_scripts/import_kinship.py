a6cde16149273696dc2e1a64f3ebc449a21de67a
filippob <filippo.biscarini@gmail.com>
2022-06-03 11:15:48 +0200

    resetting back from download_files2()

--- python_scripts/import_kinship.py
+++ python_scripts/import_kinship.py
@@ -68,8 +68,7 @@ print('Data will be downloaded to:', base_dir)
 # Kinship matrices are read from gzipped csv files, and then stacked together in a 3D array
 
 make_filenames()
-#download_files(target_dir=base_dir, remote_data_folder=remote_data_folder)
-download_files2(target_dir=base_dir, remote_data_folder=remote_data_folder)
+download_files(target_dir=base_dir, remote_data_folder=remote_data_folder)
 kinship = stack_kinship(base_dir=base_dir)
 
 print("the object 'kinship' has been created, with dimensions {}".format(kinship.shape))
